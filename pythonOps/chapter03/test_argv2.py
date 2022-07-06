3850ac03983f92bff34a93dbf3d9d92d862bbc0a
xsc <njuxushuchi@163.com>
2022-07-06 16:34:33 +0800

    update

--- pythonOps/chapter03/test_argv2.py
+++ pythonOps/chapter03/test_argv2.py
@@ -4,7 +4,7 @@ import sys
 
 def main():
     sys.argv.append("")
-    filename = sys.argv[1] # 空字符串防止不写参数，导致索引越界
+    filename = sys.argv[1]  # 空字符串防止不写参数，导致索引越界
     if not os.path.isfile(filename):
         raise SystemExit(filename + ' does not exists')
     elif not os.access(filename, os.R_OK):
